c3b50a5e926ba258d94d1739b0f2f2f5995c46b0
Tucker Rosebrock <h.rosebrock@gmail.com>
2021-03-05 13:53:51 -0500

    remove pesky consolelog

--- src/App.js
+++ src/App.js
@@ -6,14 +6,12 @@ const MBTA_BASE_URL = 'https://api-v3.mbta.com/';
 
 /**
   * Generic function to hit the MBTA API with a specified formatted URL query path.
-  * There's definitely more to be done here w/r/t ensuring our queryPath is correct.
   * 
   * @param {str} queryPath - formatted URl path
   * 
   * @returns {Promise} JSON-encoded Response from API
   */
 export const fetchMBTAData = (queryPath) => {
-  // console.log("Hello!")
   return fetch(`${MBTA_BASE_URL}${queryPath}`)
     .then(res => res.json())
     .catch(err => console.error(err))
